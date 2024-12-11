1067b676129ef6cc3efa45b13bdc6094dada2858
9erikSantos6 <9xerikx6@gmail.com>
2024-12-11 10:06:55 -0300

    feat: Add sigla, numero de legenda em Partido

--- src/br/ifpi/urna/partido/Partido.java
+++ src/br/ifpi/urna/partido/Partido.java
@@ -2,15 +2,30 @@ package br.ifpi.urna.partido;
 
 public class Partido {
     private String nome;
-    private String numeroRegistro;
+    private String sigla;
+    private String numeroLegenda;
     private int TotalVotos;
 
-    public Partido(String nome, String numeroRegistro) {
+    public Partido(String nome, String sigla, String numeroLegenda) {
         this.nome = nome;
-        this.numeroRegistro = numeroRegistro;
+        this.sigla = sigla.toUpperCase();
+        this.numeroLegenda = this.validarLegenda(numeroLegenda);
     }
 
-    public void adicionarVotos() {
+    private String validarLegenda(String numeroLegenda) {
+      if (numeroLegenda != null && numeroLegenda.matches("\\d{2}")) {
+        int intLegenda = Integer.parseInt(numeroLegenda);
+        if (10 <= intLegenda && intLegenda <= 90) {
+          return numeroLegenda;
+        } else {
+          throw new IllegalArgumentException("Número innválido para Partido.numeroLegenda: deve estar entre 10 a 90");
+        }
+      } else {
+        throw new IllegalArgumentException("Número inválido para Partido.numeroLegenda: deve ter exatamente dois dígitos.");
+      }
+    }
+
+    public void adicionarVoto() {
         this.TotalVotos++;
     }
 
@@ -18,22 +33,31 @@ public class Partido {
         this.TotalVotos = 0;
     }
 
-    public String getNome() {
-        return nome;
-    }
-
+   
     // Gets e Sets:
+
+    public String getNome() {
+      return nome;
+    }
 
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    public String getNumeroRegistro() {
-        return numeroRegistro;
+    public String getSigla() {
+        return sigla;
     }
 
-    public void setNumeroRegistro(String numeroRegistro) {
-        this.numeroRegistro = numeroRegistro;
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public String getNumeroLegenda() {
+        return numeroLegenda;
+    }
+
+    public void setNumeroLegenda(String numeroLegenda) {
+        this.numeroLegenda = numeroLegenda;
     }
 
     public int getTotalVotos() {
